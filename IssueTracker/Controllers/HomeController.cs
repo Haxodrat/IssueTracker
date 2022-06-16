133bea10954a5483ad9998280fc0f55188dcdde7
Ryan Tobin <83369913+tobinryan@users.noreply.github.com>
2022-06-16 14:56:58 -0700

    completed layout/updating reg/login

--- IssueTracker/Controllers/HomeController.cs
+++ IssueTracker/Controllers/HomeController.cs
@@ -43,7 +43,7 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult ViewTicket()
+    public IActionResult ViewTickets()
     {
         return View();
     }
