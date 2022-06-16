16fafe07283af832b17baa37e9966cfdab73e287
Ryan Tobin <83369913+tobinryan@users.noreply.github.com>
2022-06-15 22:08:06 -0700

    updated with authentication

---
+++ IssueTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace IssueTracker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
