6c9f3a4c85a434ffc6e717b7486724079bdafdcb
Ryan Tobin <83369913+tobinryan@users.noreply.github.com>
2022-06-20 22:10:24 -0700

    finished my projects tab and added create projects page

--- IssueTracker/Controllers/HomeController.cs
+++ IssueTracker/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult CreateProjects()
+    {
+        return View();
+    }
+
     public IActionResult Projects()
     {
         return View();
