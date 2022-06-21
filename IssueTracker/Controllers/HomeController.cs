0df471fb0a690c6d75f8357c1e846535b106b667
Ryan Tobin <83369913+tobinryan@users.noreply.github.com>
2022-06-21 15:38:10 -0700

    finished project datatable and created view for project details

--- IssueTracker/Controllers/HomeController.cs
+++ IssueTracker/Controllers/HomeController.cs
@@ -58,6 +58,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Details()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
