c2f24244260eed9761d2084df22212d9e6cee075
Ryan Tobin <83369913+tobinryan@users.noreply.github.com>
2022-07-29 20:37:23 -0700

    claims principal factory extension class

---
+++ IssueTracker/Models/ApplicationUserClaimsPrincipalFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using IssueTracker.Areas.Identity.Data;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+
+namespace IssueTracker.Models
+{
+    public class ApplicationUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<ApplicationUser>
+    {
+        public ApplicationUserClaimsPrincipalFactory(UserManager<ApplicationUser> userManager,
+            IOptions<IdentityOptions> options) : base(userManager, options)
+        {
+        }
+
+        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(ApplicationUser user)
+        {
+            var identity = await base.GenerateClaimsAsync(user);
+            identity.AddClaim(new Claim("FirstName", user.FirstName ?? ""));
+            identity.AddClaim(new Claim("LastName", user.LastName ?? ""));
+            return identity;
+        }
+    }
+}
+
