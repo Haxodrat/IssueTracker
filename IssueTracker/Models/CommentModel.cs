5d0480a73f59cf32e767d5cd4fa1037e915f3948
Christopher Kim <haxodrat@icloud.com>
2022-08-09 23:13:32 -0700

    still need to fix the comment migrations

---
+++ IssueTracker/Models/CommentModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using IssueTracker.Areas.Identity.Data;
+
+namespace IssueTracker.Models
+{
+	public class CommentModel
+	{
+		
+		public int ID { get; set; }
+
+		[Required]
+		[Display(Name = "Content")]
+		public string? Content { get; set; }
+
+		[Required]
+		[Display(Name = "User")]
+		public virtual ApplicationUser User { get; set; }
+
+		[Required]
+		[Display(Name = "Ticket")]
+		public virtual TicketModel Ticket { get; set; }
+
+
+	}
+}
+
